493cef234c4892f920954e957a5be8511d96b6ae
Renan Duarte <renandg.axl@gmail.com>
2022-02-22 19:06:36 -0300

    Create a new genre

--- movieflix/src/main/java/com/devsuperior/movieflix/services/GenreService.java
+++ movieflix/src/main/java/com/devsuperior/movieflix/services/GenreService.java
@@ -9,27 +9,52 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.movieflix.dto.GenreDTO;
+import com.devsuperior.movieflix.dto.MovieDTO;
 import com.devsuperior.movieflix.entities.Genre;
+import com.devsuperior.movieflix.entities.Movie;
 import com.devsuperior.movieflix.repositories.GenreRepository;
+import com.devsuperior.movieflix.repositories.MovieRepository;
 
 @Service
 public class GenreService {
-	
+
 	@Autowired
 	private GenreRepository repository;
-	
+
+	@Autowired
+	private MovieRepository movieRepository;
+
 	@Transactional(readOnly = true)
-	public Page<GenreDTO> findAllPaged(PageRequest pageRequest){
+	public Page<GenreDTO> findAllPaged(PageRequest pageRequest) {
 		Page<Genre> page = repository.findAll(pageRequest);
-		
+
 		return page.map(x -> new GenreDTO(x, x.getMovies()));
 	}
-	
+
 	@Transactional(readOnly = true)
 	public GenreDTO findById(Long id) {
 		Optional<Genre> obj = repository.findById(id);
 		Genre entity = obj.get();
 		return new GenreDTO(entity, entity.getMovies());
+	}
+	
+	@Transactional
+	public GenreDTO insert(GenreDTO dto) {
+		Genre entity = new Genre();
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+		return new GenreDTO(entity, entity.getMovies());
+	}
+
+	private void copyDtoToEntity(GenreDTO dto, Genre entity) {
+		entity.setName(dto.getName());
+
+		entity.getMovies().clear();
+
+		for (MovieDTO movDto : dto.getMovies()) {
+			Movie movie = movieRepository.getOne(movDto.getId());
+			entity.getMovies().add(movie);
+		}
 	}
 
 }
