de8748169e41cbb3e55e2e21f15a6786524ced1a
Renan Duarte <renandg.axl@gmail.com>
2022-03-16 11:34:55 -0300

    List movies by title using JPQL

--- movieflix/src/main/java/com/devsuperior/movieflix/services/MovieService.java
+++ movieflix/src/main/java/com/devsuperior/movieflix/services/MovieService.java
@@ -1,5 +1,7 @@
 package com.devsuperior.movieflix.services;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import javax.persistence.EntityNotFoundException;
@@ -14,8 +16,10 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.movieflix.dto.MovieDTO;
 import com.devsuperior.movieflix.dto.ReviewDTO;
+import com.devsuperior.movieflix.entities.Genre;
 import com.devsuperior.movieflix.entities.Movie;
 import com.devsuperior.movieflix.entities.Review;
+import com.devsuperior.movieflix.repositories.GenreRepository;
 import com.devsuperior.movieflix.repositories.MovieRepository;
 import com.devsuperior.movieflix.repositories.ReviewRepository;
 import com.devsuperior.movieflix.services.exceptions.DatabaseException;
@@ -29,11 +33,14 @@ public class MovieService {
 
 	@Autowired
 	private ReviewRepository reviewRepository;
+	
+	@Autowired
+	private GenreRepository genreRepository;
 
 	@Transactional(readOnly = true)
-	public Page<MovieDTO> findAllPaged(PageRequest pageRequest) {
-		Page<Movie> page = repository.findAll(pageRequest);
-
+	public Page<MovieDTO> findAllPaged(Long genreId, String title, PageRequest pageRequest) {
+		Genre genre = (genreId == 0L) ? null : genreRepository.getOne(genreId);
+		Page<Movie> page = repository.find(genre, title, pageRequest);
 		return page.map(x -> new MovieDTO(x, x.getReviews()));
 	}
 
